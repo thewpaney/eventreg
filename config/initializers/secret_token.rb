4676bf887d062d882b67001eebc513c69be8dc99
Matthew Haney <mahaney@q.com>
2012-09-21 22:47:25 -0600

    * Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spr::Application.config.secret_token = '1752e9832018a1f9a7515d5710072f4b9e39d5ca50f888e9fcac37513dfd76bca99d38f96aeab77c97239fe8148d3c2bb58defd6e7148744af0c8e9c2e36c508'
---
+++ db/migrate/20120921223829_create_users.rb
@@ -0,0 +1,24 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.column :name, :string
+      t.column :student_id, :number
+      t.column :session1, :number
+      t.column :session2, :number
+    end
+  end
+
+  def change
+    create_table :users do |t|
+      t.column :name, :string
+      t.column :student_id, :number
+      t.column :session1, :number
+      t.column :session2, :number
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+
+end
