9ca8a7b67250fc1c12ae7cf81090e089a4073b29
Matthew Haney <mahaney@q.com>
2012-09-23 15:53:27 -0600

    * Stuff?

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,14 @@ class Event < ActiveRecord::Base
 
   has_many :users
 
+  def self.generate_html_list
+    str = ""
+    Event.all.sort {|a,b| a.name <=> b.name }.each do |e|
+      str += "<tr><td>#{e.name} - #{e.users.count}/#{e.maxcapacity}\n"
+    end
+    return str
+  end
+
   def self.is_available?(sessionid)
     e = find(:first, :conditions=>["id=?", sessionid])
     return false if e.nil?
