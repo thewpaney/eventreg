1ff6263846ac248b0b2b49dedc4a8e3153b96bbf
Matthew Haney <mahaney@q.com>
2012-09-23 14:34:20 -0600

    * Events.yml

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,8 +1,17 @@
 class EventController < ApplicationController
-  def register
-    if request.post?
-      params[:event][:sessionid]
-      redirect_to :controller => "user", :action => "login"
+
+  before_filter :authenticate
+  
+  def review
+    redirect_to :controller => "user", :action => "login"
+  end
+  
+  private
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == USER_NAME && password == PASSWORD
     end
   end
+  
+
 end
