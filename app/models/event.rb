852cb3a992b730df9a84766eb10d22cbc1e77ae2
Matthew Haney <mahaney@q.com>
2012-09-23 12:06:52 -0600

    * Pre-Sunday modifications

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,24 +1,24 @@
 class Event < ActiveRecord::Base
-  attr_accessible :maxcapacity, :name, :sessionid
-  
-  attr_accessible :currentcapacity
-  
+  attr_accessible :maxcapacity, :name
+
+  has_many :users
+
   def self.is_available?(sessionid)
     e = find(:first, :conditions=>["id=?", sessionid])
     return false if e.nil?
-    return true if e.currentcapacity < e.maxcapacity
+    return true if e.users.count < e.maxcapacity
     return false
   end
 
-  def self.register_event(sessionid)
-    e = find(:first, :conditions=>["id=?", sessionid])
-    e.currentcapacity += 1 if is_available?(sessionid)
-    e.save
-  end
+#  def self.register_event(sessionid)
+#    e = find(:first, :conditions=>["id=?", sessionid])
+#    e.currentcapacity += 1 if is_available?(sessionid)
+#    e.save
+#  end
 
-  def self.unregister_event(sessionid)
-    e = find(:first, :conditions=>["id=?", sessionid])
-    e.currentcapacity -= 1
-    e.save
-  end
+#  def self.unregister_event(sessionid)
+#    e = find(:first, :conditions=>["id=?", sessionid])
+#    e.currentcapacity -= 1
+#    e.save
+#  end
 end
