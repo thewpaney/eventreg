4a3029d55caca76a38fdb265aabde3cbf7c30bb1
Matthew Haney <mahaney@q.com>
2012-09-26 13:25:03 -0600

    * Added lots of fun stuff.  Lunch changes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,16 @@ Spr::Application.routes.draw do
   get "user/login"
   get "user/logout"
   get "event/review"
-
+  get "event/admin"
+  get "event/export"
+  get "event/edit_events"
+  get "event/edit_users"
+  
   post "user/register_event"
   post "user/login"
   post "user/edit_sessions"
+  post "event/edit_events"
+  post "event/edit_users"
 
   root :to => "user#login"
 
