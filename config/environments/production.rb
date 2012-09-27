60366009bba4a712e7da5ed829965ad66043d202
Morgan Jones <integ3rs@gmail.com>
2012-09-27 11:28:09 -0500

    * Mess with namespaces for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Spr::Application.configure do
+ER::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
