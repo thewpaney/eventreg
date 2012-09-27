8ed237844b6a6f1b9ea9a08eadf35b544b57978f
Matthew Haney <mahaney@q.com>
2012-09-27 14:47:54 -0600

    * We needed a secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ER::Application.config.secret_token = 'changeme'
+ER::Application.config.secret_token = '289c7106eab1f51ce40e193661c7fd70andabucketofdd603ff60bc36be174370961137466e6withyourmothers4a23d62be1597b01c5df3bb46d1235a9'
