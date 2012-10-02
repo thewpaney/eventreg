168c82e100603c3c11dfeab3171f69754d2775db
Matthew Haney <mahaney@q.com>
2012-10-01 22:49:45 -0600

    * Changes to things!

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,33 +5,15 @@ class User < ActiveRecord::Base
 
   validates :student_id, :length => { :is => 5 }
 
-  attr_accessor :student_id, :name
-
   belongs_to :event
 
   def self.authenticate(n, pass)
-    u = User.find(:first, :conditions=>["name = ?", n])
-    if u.nil?
-      return nil
-    end
-    if u.attributes["student_id"] == pass.to_i
-      return u
-    end
-    nil
+    u = (User.where(name: n).first).student_id == pass.to_i ? u : nil
+    #u = User.where(name: n).first
+    #u = User.find(:first, :conditions=>["name = ?", n])
+    #return nil if u.nil?
+    #return u if u.student_id == pass.to_i
+    #nil
   end  
-  
-  # def self.store(session1, session2)
-  #   u = find(:first, :conditions=>["name=? AND id=?", name, pass])
-  #   unless u.nil?
-  #     u.update_attributes
-  #   else
-  #     flash[:error] = "That operation requires you to be logged in."
-  #   end
-  # end
-
-  # def self.eventid(name, pass)
-  #   u = find(:first, :conditions=>["name=? AND id=?", name, pass])
-  #   return u.sessionid
-  # end
   
 end
