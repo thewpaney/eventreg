df3201570c8c0bf0da9b6f6960519cb7eb3fdc50
Morgan Jones <integ3rs@gmail.com>
2012-10-05 11:16:50 -0500

    * Time zone fixes

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -1,5 +1,4 @@
 ER::Application.config.time = {
-  9 => {open: Time.now, close: Time.now + 1.month},
-  11 => {open: Time.utc(2012, 10, 17, 20, 0), close: Time.utc(2012, 10, 19, 20, 0)},
-  12 => {open: Time.utc(2012, 10, 7, 20, 0), close: Time.utc(2012, 10, 9, 20, 0)}
+  11 => {open: Time.utc(2012, 10, 18, 2, 0), close: Time.utc(2012, 10, 20, 2, 0)},
+  12 => {open: Time.utc(2012, 10, 8, 2, 0), close: Time.utc(2012, 10, 10, 2, 0)}
 }
