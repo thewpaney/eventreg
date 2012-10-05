9957730c0264c417de0501c84c33bc8ea067c3da
Morgan Jones <integ3rs@gmail.com>
2012-10-05 12:03:18 -0500

    * add slash

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,5 +1,5 @@
 module EventHelper
   def self.generate_html_list
-    Event.all.sort {|a, b| a.name <=> b.name }.collect {|e| "<tr><td>#{e.name.html_safe}</td><td>#{e.users.count}</td><td>#{e.capacity}</td></tr>" + e.users.collect{|u| "<tr><td></td><td></td><td>#{u.name.html_safe}</td></tr>"}.join}.join
+    Event.all.sort {|a, b| a.name <=> b.name }.collect {|e| "<tr><td>#{e.name.html_safe}</td><td><strong>#{e.users.count}/#{e.capacity}</strong></td></tr>" + e.users.collect{|u| "<tr><td></td><td></td><td>#{u.name.html_safe}</td></tr>"}.join}.join
   end
 end
