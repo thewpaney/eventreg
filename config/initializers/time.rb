bbe3280111ab5d816ed1931a57704c392c569f9e
Morgan Jones <integ3rs@gmail.com>
2012-10-05 17:52:28 -0500

    * Remove test time from the initializers * User controller: Take into account the `restrict` option (note: I actually updated this on the production server too)

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -1,5 +1,4 @@
 ER::Application.config.time = {
-  9 => {open: Time.now, close: Time.at(9999999999)},
   11 => {open: Time.utc(2012, 10, 18, 2, 0), close: Time.utc(2012, 10, 20, 2, 0)},
   12 => {open: Time.utc(2012, 10, 8, 2, 0), close: Time.utc(2012, 10, 10, 2, 0)}
 }
