d091a74c0ad948bd8bf8f8d44c16013fc8a12356
Morgan Jones <integ3rs@gmail.com>
2012-10-05 12:45:54 -0500

    * Remove unimplemented things.

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,9 @@ ER::Application.routes.draw do
   post '/user/login' => 'user#login'
   post '/user/edit' => 'user#update'
 
-  get '/event/review' => 'event#review'
   get '/event/admin' => 'event#admin'
+  get '/event/review' => 'event#review'
   get '/event/export' => 'event#export'
-  get '/event/edit' => 'event#edit_events'
-  get '/event/users' => 'event#edit_users'
-  post '/event/edit' => 'event#save_events'
-  post '/event/edit_users' => 'event#save_users'
 
   root :to => 'user#login'
 end
