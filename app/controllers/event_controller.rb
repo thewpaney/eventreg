6f9ba38748d5dee04b20d451104dcba003a9373e
Morgan Jones <integ3rs@gmail.com>
2012-10-06 19:00:06 -0500

    * Sessions should be stored in the database. They were getting too big for cookies. * Moving stuff around. Verified ALL corner cases.

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -13,15 +13,17 @@ class EventController < ApplicationController
 
   def edit_events
     return unless request.post?
-    if params[:event][:id] == ""
+    if params[:event][:id].empty?
       flash[:error] = "Please select a site to modify."
-      return
     else 
-      e = Event.where(id: params[:event][:id])
-      e.first.name = params[:event][:name] unless params[:event][:name] == ""
-      e.first.capacity = params[:event][:capacity].to_i unless params[:event][:capacity] == ""
-      e.first.save!
-      flash[:message] = "Event successfully updated."
+      e = Event.find(params[:event][:id].to_i)
+      e.first.name = params[:event][:name] unless params[:event][:name].empty?
+      e.first.capacity = params[:event][:capacity].to_i unless params[:event][:capacity].empty
+      if e.first.save
+        flash[:message] = "Event successfully updated."
+      else
+        flash[:message] = "Error modifying event."
+      end
     end
   end
 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,17 +7,20 @@ class UserController < ApplicationController
   caches_page :login
 
   POSITIVE_ENCOURAGEMENT = [
-                            "That's all you have to do!"
+                            "That's all you have to do!",
+                            "You're done!",
+                            "That's it! Seriously!",
+                            "Congratulations!"
                            ]
 
   NEGATIVE_ENCOURAGEMENT = [
                    "The one that got away!",
-                   "Aww man!"
+                   "Aww man!",
+                   "Hope you had a backup!"
                    ]
 
-
   def check_open
-    if is_time?
+    if user.is_time?
       redirect_to action: 'register'
     else
       flash[:error] = "Registration is not yet open."
@@ -28,10 +31,6 @@ class UserController < ApplicationController
     @event = !params[:event].nil? && !params[:event][:id].nil? ? Event.where(id: params[:event][:id]).first : nil
   end
 
-  def is_time?
-    !(g = user.registration).nil? && ((t = Time.now.utc) - g[:open] > 0) && (t - g[:close] < 0)
-  end
-
   def ready
     # silence is golden
   end
@@ -40,7 +39,7 @@ class UserController < ApplicationController
     if !user?
       flash[:error] = "You must be logged in to register."
       redirect_to action: 'login'
-    elsif !is_time?
+    elsif !user.is_time?
       flash[:error] = "Registration hasn't opened yet!"
       redirect_to action: 'ready'
     end
@@ -52,10 +51,13 @@ class UserController < ApplicationController
     if @event.nil?
       flash[:error] = "Please pick an option."
     else
-      if @event.restrict == 0 || @event.restrict == user.grade 
+      if user.can_register?(@event) 
         if @event.available?
-          self.user.update_attributes event: @event
-          flash[:message] = "You're now registered for #{@event}! #{POSITIVE_ENCOURAGEMENT.sample}"
+          if self.user.update_attributes(event: @event)
+            flash[:message] = "You're now registered for #{@event}! #{POSITIVE_ENCOURAGEMENT.sample}"
+          else
+            flash[:error] = "Couldn't register you for this event. Try another one, maybe?"
+          end
         else
           flash[:error] = "No spots left for #{@event}! #{NEGATIVE_ENCOURAGEMENT.sample}"
         end
@@ -68,7 +70,7 @@ class UserController < ApplicationController
   def login
     self.authenticate! params[:user]
     if self.user?
-      flash[:message]  = "You are logged in as #{user.name}."
+      flash[:message]  = "You're logged in as #{user.name}."
       redirect_to action: "ready"
     elsif request.post?
       flash[:error] = "Login failed."
@@ -80,7 +82,7 @@ class UserController < ApplicationController
       self.deauthenticate!
       flash[:message] = 'Successfully logged out.'
     else
-      flash[:error] = "You are not currently logged in!"
+      flash[:error] = "You're not currently logged in!"
     end
     redirect_to action: 'login'
   end
