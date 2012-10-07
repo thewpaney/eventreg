5e851621b5bc60144fa965371af848b9f69c57ef
Matthew Haney <mahaney@q.com>
2012-10-07 11:53:18 -0600

    * There we go.

--- config/initializers/admin.rb
+++
@@ -1,5 +0,0 @@
-ER::Application.config.admin = {
-  message: 'Log in to continue',
-  username: 'profe',
-  password: 'watchmen02'
-}
--- config/initializers/time.rb
+++
@@ -1,6 +0,0 @@
-ER::Application.config.time = {
-  9  => {open: Time.now, close: Time.now + 1.month},
-  10 => {open: Time.now, close: Time.now + 1.month},
-  11 => {open: Time.now, close: Time.now + 1.month},
-  12 => {open: Time.now, close: Time.now + 1.month}
-}
