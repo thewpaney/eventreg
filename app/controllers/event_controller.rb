ee01be280cdea4677c51494d7d5898297c801a6a
Matthew Haney <mahaney@q.com>
2012-10-17 20:15:26 -0600

    * Fixed edit_sessions * Work on editing users

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -29,10 +29,10 @@ class EventController < ApplicationController
       Event.find(params[:event][:id].to_i).destroy
       flash[:message] = "Event removed."
     else 
-      e = Event.find(params[:event][:id].to_i)
+      e = Event.where(:id => params[:event][:id].to_i)
       e.first.name = params[:event][:name] unless params[:event][:name].empty?
-      e.first.capacity = params[:event][:capacity].to_i unless params[:event][:capacity].empty
-      if e.first.save
+      e.first.capacity = params[:event][:capacity].to_i unless params[:event][:capacity].empty?
+      if e.first.save!
         flash[:message] = "Event successfully updated."
       else
         flash[:message] = "Error modifying event."
