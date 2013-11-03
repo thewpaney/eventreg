a9a28f48b7bca941274f5a63dea08529a6131722
Matthew Haney <mahaney@q.com>
2013-11-03 11:39:47 -0700

    * Todo.org is back! * Godmode placeholder * Mailer placeholder

---
+++ app/mailers/eventreg_mailer.rb
@@ -0,0 +1,31 @@
+class EventregMailer < ActionMailer::Base
+  default from: "Diversity Day Registrar <mailer@diversity.stratosphe.re>"
+
+#  def game_end_email_tagged(player)
+#    @player = player
+#    mail(to: player.email, subject: "RJ Spoon Wars")
+#  end
+
+#  def game_end_email_untagged(player)
+#    @player = player
+#    mail(to: player.email, subject: "RJ Spoon Wars -- Round Two End")
+#  end
+  
+#  def game_open_email(player)
+#    @player = player
+#    @url = 'spoons.stratosphe.re'
+#    mail(to: player.email, subject: "RJ Spoon Wars")
+#  end
+
+#  def tag_confirm_email(player, tag)
+#    @player = player
+#    @tag = tag
+#    mail(to: player.email, subject: "RJ Spoon Wars -- Tag Confirmation")
+#  end
+
+#  def mobile_alert(player)
+#    @player = player
+#    mail(to: player.email, subject: "RJ Spoon Wars -- Mobile Devices")
+#  end
+
+end
