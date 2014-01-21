8e870f673ef89ea3c64a8900cdb53a2aefd3815f
Matthew Haney <mahaney@q.com>
2014-01-20 18:24:22 -0700

    * Mailer stuff!  Yay!

--- app/mailers/eventreg_mailer.rb
+++ app/mailers/eventreg_mailer.rb
@@ -1,31 +1,25 @@
 class EventregMailer < ActionMailer::Base
   default from: "Diversity Day Registrar <mailer@diversity.stratosphe.re>"
 
-#  def game_end_email_tagged(player)
-#    @player = player
-#    mail(to: player.email, subject: "RJ Spoon Wars")
-#  end
+  def late_email(user)
+    @user = user
+    mail(to: user.email, subject: "RJ Diversity Day Registration Closing")
+  end
 
-#  def game_end_email_untagged(player)
-#    @player = player
-#    mail(to: player.email, subject: "RJ Spoon Wars -- Round Two End")
-#  end
-  
-#  def game_open_email(player)
-#    @player = player
-#    @url = 'spoons.stratosphe.re'
-#    mail(to: player.email, subject: "RJ Spoon Wars")
-#  end
+  def register_confirmation_email(user)
+    @user = user
+    mail(to: user.email, subject: "RJ Diversity Day Registration Confirmation")
+  end
 
-#  def tag_confirm_email(player, tag)
-#    @player = player
-#    @tag = tag
-#    mail(to: player.email, subject: "RJ Spoon Wars -- Tag Confirmation")
-#  end
+  def admin_blast_email(user, subject, body)
+    @user = user
+    @body = body
+    mail(to: user.email, subject: subject)
+  end
 
-#  def mobile_alert(player)
-#    @player = player
-#    mail(to: player.email, subject: "RJ Spoon Wars -- Mobile Devices")
-#  end
+  def open_email(user)
+    @user = user
+    mail(to: user.email, subject: "RJ Diversity Day Registration Now Open")
+  end
 
 end
