dd17113453312d750316d3a86abfceeb2a0ff662
Matthew Haney <mahaney@q.com>
2014-02-23 08:34:16 +0000

    * Mailer progress and some config settings

--- app/mailers/eventreg_mailer.rb
+++ app/mailers/eventreg_mailer.rb
@@ -1,3 +1,16 @@
 class EventregMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "diversity.mailer@gmail.com"
+  
+  def open_email(recipient)
+    mail(to: recipient.email, subject: "RJHS Diversity Day Registration Now Open!")
+  end
+
+  def late_email(recipient)
+    
+  end
+
+  def register_confirmation_email(recipient)
+    
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.delivery_method = :sendmail
+
+ActionMailer::Base.sendmail_settings = { :address => "smtp.gmail.com",
+     :port => "587", :domain => "gmail.com", :user_name => "diversity.mailer@gmail.com", 
+  :password => "kurdmcfrogspleen", :authentication => "plain", :enable_starttls_auto => true }
+
 # Initialize the Rails application.
 Eventreg::Application.initialize!
+
