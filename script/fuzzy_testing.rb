1ff559b1f60f06b8fb430d370b370cd4d244666f
Drew Lazzeri <drew@interviewing.io>
2018-02-22 22:35:06 -0800

    Updated todo

--- script/fuzzy_testing.rb
+++ script/fuzzy_testing.rb
@@ -1,22 +1,8 @@
-Student.all.shuffle do |student| 
-  begin
-    first  = Workshop.firstsAvailable(student).sample
-    student.signup first.id
-  rescue
-    puts "Unable to place #{student.prefix} in #{first.name}"
-  end
-
-  begin
-    second = Workshop.secondsAvailable(student).sample
-    student.signup second.id
-  rescue
-    puts "Unable to place #{student.prefix} in #{second.name}"
-  end
-
-  begin
-    third  = Workshop.thirdsAvailable(student).sample
-    student.signup third.id
-  rescue
-    puts "Unable to place #{student.prefix} in #{second.name}"
-  end
+Student.all.shuffle.each do |student|
+  first  = Workshop.firstsAvailable(student).sample
+  student.signup first.id
+  second = Workshop.secondsAvailable(student).sample
+  student.signup second.id
+  third  = Workshop.thirdsAvailable(student).sample
+  student.signup third.id
 end
