7b2bdfb360b08c5185471ee45ae10c811a37f5dc
Matthew Haney <thewpaney@gmail.com>
2018-02-25 19:03:24 -0700

    Updated presenter seeding to use split worksheets.

--- db/seeds/presenters.rb
+++ db/seeds/presenters.rb
@@ -4,20 +4,21 @@ $workshop_url_2018 = "1Ww5qmj89gYDIdB3OCjpM8fvhkFo50mTADvf3eSAjqLQ"
 
 session = GoogleDrive::Session.from_config("config.json")
 
-puts "Downloading presenters"
-presenters = session.spreadsheet_by_key($workshop_url_2018).worksheets[3]
-puts "Downloaded presenters"
+puts "Downloading spreadsheet"
+spreadsheet = session.spreadsheet_by_key($workshop_url_2018)
+puts "Downloaded spreadsheet"
 
-presenters.rows[1..-1].each do |row|
+teacher_presenters = spreadsheet.worksheets[2]
+student_presenters = spreadsheet.worksheets[3]
+
+puts "Seeding student presenters"
+student_presenters.rows[1..-1].each do |row|
   p = Student.where(full: row[0]).first
-  if p.nil?
-    p = Teacher.where(name: row[0]).first
-  end
   if p.nil?
     puts "Bad presenter name: #{row[0]}"
     next
   end
-  if row[1] == "x"
+  if row[1].include? "1"
     w = Workshop.where(name: row[4], session: 1).first
     if w.nil?
       puts "No workshop named #{row[4]} in session 1"
@@ -30,7 +31,7 @@ presenters.rows[1..-1].each do |row|
       puts "Signed up #{p.full} for workshop #{w.name} in session #{w.session}."
     end
   end
-  if row[2] == "x"
+  if row[1].include? "2"
     w = Workshop.where(name: row[4], session: 2).first
     if w.nil?
       puts "No workshop named #{row[4]} in session 1"
@@ -43,7 +44,7 @@ presenters.rows[1..-1].each do |row|
       puts "Signed up #{p.full} for workshop #{w.name} in session #{w.session}."
     end
   end
-  if row[3] == "x"
+  if row[1].include? "3"
     w = Workshop.where(name: row[4], session: 3).first
     if w.nil?
       puts "No workshop named #{row[4]} in session 1"
@@ -59,4 +60,55 @@ presenters.rows[1..-1].each do |row|
   p.save!
 end
 
-puts "Seeded presenters"
+puts "Seeded student presenters"
+
+puts "Seeding teacher presenters"
+teacher_presenters.rows[1..-1].each do |row|
+  p = Teacher.where(name: row[0]).first
+  if p.nil?
+    puts "Bad presenter name: #{row[0]}"
+    next
+  end
+  if row[1].include? "1"
+    w = Workshop.where(name: row[2], session: 1).first
+    if w.nil?
+      puts "No workshop named #{row[2]} in session 1"
+      next
+    end
+    if p.workshops.include? w
+      puts "#{p.full} already signed up for #{w.name} in session #{w.session}"
+    else
+      p.workshops << w
+      puts "Signed up #{p.full} for workshop #{w.name} in session #{w.session}."
+    end
+  end
+  if row[1].include? "2"
+    w = Workshop.where(name: row[2], session: 2).first
+    if w.nil?
+      puts "No workshop named #{row[2]} in session 1"
+      next
+    end
+    if p.workshops.include? w
+      puts "#{p.full} already signed up for #{w.name} in session #{w.session}"
+    else
+      p.workshops << w
+      puts "Signed up #{p.full} for workshop #{w.name} in session #{w.session}."
+    end
+  end
+  if row[1].include? "3"
+    w = Workshop.where(name: row[2], session: 3).first
+    if w.nil?
+      puts "No workshop named #{row[2]} in session 1"
+      next
+    end
+    if p.workshops.include? w
+      puts "#{p.full} already signed up for #{w.name} in session #{w.session}"
+    else
+      p.workshops << w
+      puts "Signed up #{p.full} for workshop #{w.name} in session #{w.session}."
+    end
+  end
+  p.save!
+end
+
+puts "Seeded teacher presenters"
