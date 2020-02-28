f7766e3e63dd334efa324b3991a3eca82b58ccaf
Matthew Haney <mhaney@icron.com>
2020-02-28 14:56:08 -0700

    Fix copy-paste error

--- db/migrate/20200228194925_change_limit_fields_to_integers.rb
+++ db/migrate/20200228194925_change_limit_fields_to_integers.rb
@@ -1,11 +1,11 @@
 class ChangeLimitFieldsToIntegers < ActiveRecord::Migration[6.0]
   def up
-    change_column :workshops, :tlimit, 'integer USING CAST(age AS integer)'
-    change_column :workshops, :slimit, 'integer USING CAST(age AS integer)'
+    change_column :workshops, :tlimit, 'integer USING CAST(tlimit AS integer)'
+    change_column :workshops, :slimit, 'integer USING CAST(slimit AS integer)'
   end
 
   def down
-    change_column :workshops, :tlimit, 'string USING CAST(age AS string)'
-    change_column :workshops, :slimit, 'string USING CAST(age AS string)'
+    change_column :workshops, :tlimit, 'string USING CAST(tlimit AS string)'
+    change_column :workshops, :slimit, 'string USING CAST(slimit AS string)'
   end
 end
